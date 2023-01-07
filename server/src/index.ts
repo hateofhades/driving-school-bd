419743dd9e4c5724ea5e95f05f8d8159ff5e36a9
Serban Andrei <usadekall@gmail.com>
2023-01-07 12:05:18 +0200

    Added changePassword and deleteAccount

--- server/src/index.ts
+++ server/src/index.ts
@@ -79,6 +79,7 @@ app.post('/register', (req, res) => {
     const { username, password, email, nume, prenume, sex, datanastere, cnp, adresa, idinstructor } = req.body;
 
     const query = `INSERT INTO Conturi (Username, Parola, Email) VALUES ('${username}', '${password}', '${email}')`;
+    console.log(query);
 
     try {
         connection.query(query, (error, results, fields) => {
@@ -113,6 +114,75 @@ app.post('/register', (req, res) => {
         console.log(error);
     }
 });
+
+app.post('/changePassword', (req, res) => {
+    const { username, oldPassword, newPassword } = req.body;
+
+    const query = `UPDATE Conturi SET Parola = '${newPassword}' WHERE Username = '${username}' AND Parola = '${oldPassword}'`;
+    console.log(query);
+
+    try {
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                res.status(200).json({
+                    error: 500
+                });
+
+                console.log(error);
+                return;
+            }
+
+            if (results.affectedRows > 0) {
+                res.status(200).json({
+                    error: 0
+                });
+            } else {
+                res.status(200).json({
+                    error: 401
+                });
+            }
+        });
+    } catch (error) {
+        res.status(200).json({
+            error: 500
+        });
+    }
+});
+
+app.post('/deleteAccount', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = `DELETE FROM Conturi WHERE Username = '${username}' AND Parola = '${password}'`;
+    console.log(query);
+
+    try {
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                res.status(200).json({
+                    error: 500
+                });
+
+                console.log(error);
+                return;
+            }
+
+            if (results.affectedRows > 0) {
+                res.status(200).json({
+                    error: 0
+                });
+            } else {
+                res.status(200).json({
+                    error: 401
+                });
+            }
+        });
+    } catch (error) {
+        res.status(200).json({
+            error: 500
+        });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
